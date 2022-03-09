6d5eee5a280c8b4d02410a35764674b2fbfa4b52
Sunit Joshi <eagleamulet@gmail.com>
2022-03-09 11:57:07 -0600

    Start

---
+++ BasicTypes/CE-BasicTypes-Quiz.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  //Quiz
+/*
+Create 1 string variable called prodName with value "Pet Leash"
+Create 1 double variable called prodPrice with value 15.99
+Create 1 variable using var keyword called prodQ
+Create 1 variable using var called orderTotal that is prodPrice * prodQuantity.
+Print using variable interpolation and expression to output this:
+
+3 orders of Pet Leash cost 48!
+
+Hint: Use $variable and ${expression} to get this output. Use the round() method
+of orderTotal to round the cost.
+
+*/
+//Enter your code after this line
+}
