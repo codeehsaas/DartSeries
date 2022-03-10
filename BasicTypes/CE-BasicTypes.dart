474ad64de3e795ba147b92a2577b8187e9af3596
Code Ehsaas <codeehsaas@gmail.com>
2022-03-10 10:40:00 -0600

    Final with code examples

--- BasicTypes/CE-BasicTypes.dart
+++ BasicTypes/CE-BasicTypes.dart
@@ -15,9 +15,32 @@ void main() {
   String Interpolation using $variable and ${}
    */
 
+  String petName = 'Zyraa';
+  var petBreed = 'Ridgeback';
+  //Variable interpolation in strings
+  print('$petName is a $petBreed');
+
+  //String methods & properties
+  print('${petName.toUpperCase()} is of length ${petName.length}');
+
+  //Using escape character for \\
+  print('C:\\Windows\\System32');
+
+  //Using a raw r string
+  print(r'C:\Windows\System32\$folder');
+
   /*
   int variables - int <variable name> = <no>
   double variables - double <variable name> = <no_with_decimal_point>
    */
+  var petAge = 2;
+  double petWeight = 52.4;
 
+  print('$petName weighs $petWeight and is of age $petAge');
+
+  /*
+  bool variables can only have true or false value
+   */
+  var likesChicken = true;
+  print('$petName like chicken $likesChicken');
 }
