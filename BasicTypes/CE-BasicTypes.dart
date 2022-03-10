40f796e97c766fbdf6def2674841308729ae82ef
Code Ehsaas <codeehsaas@gmail.com>
2022-03-10 08:35:27 -0600

    Final Code Templates

--- BasicTypes/CE-BasicTypes.dart
+++ BasicTypes/CE-BasicTypes.dart
@@ -4,16 +4,17 @@ void main() {
   A Multi-line comment
  This is the second line of the comment
   /*
-
    */
   Variables - Created with <type> <variable name> = <value>
   also with: <var> <variable name> = <value>
   String variables - String <variable name> = '<value>' or "<value>"
   String Escaping - Using single quote, \, $, inside strings
   Raw String using r - Example is Path in Windows.
-   */
-
+  String methods return a new string. Original string is unchanged
   /*
+
+
+   */
   int variables - int <variable name> = <no>
   double variables - double <variable name> = <no_with_decimal_point>
    */
