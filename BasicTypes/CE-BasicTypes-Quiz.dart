55100431c57f873a03faa659edaa0714ea438c36
Code Ehsaas <codeehsaas@gmail.com>
2022-03-09 19:19:18 -0600

    With code templates

--- BasicTypes/CE-BasicTypes-Quiz.dart
+++ BasicTypes/CE-BasicTypes-Quiz.dart
@@ -8,10 +8,11 @@ Create 1 variable using var keyword called prodQ
 Create 1 variable using var called orderTotal that is prodPrice * prodQuantity.
 Print using variable interpolation and expression to output this:
 
-3 orders of Pet Leash cost 48!
+3 orders of Pet Leash cost $48!
 
 Hint: Use $variable and ${expression} to get this output. Use the round() method
-of orderTotal to round the cost.
+of orderTotal to round the cost. Also, note we're trying to print the $ sign!!
+So you may need to escape it.
 
 */
 //Enter your code after this line
--- BasicTypes/CE-BasicTypes.dart
+++ BasicTypes/CE-BasicTypes.dart
@@ -1,3 +1,32 @@
 void main() {
+  /*
+  Main - void main() { } is the entry point of any Dart program. Without it a Dart
+  program cannot run.
+   */
 
+  //Comments start with 2 forward slashes
+  /*
+  Multi-line comments use /* ... */
+   */
+
+  /*
+  Variables - Created with <type> <variable name> = <value>
+  also with: <var> <variable name> = <value>
+  String variables - String <variable name> = '<value>' or "<value>"
+  String Escaping - Using single quote, \, $, inside strings
+  Raw String using r - Example is Path in Windows.
+   */
+
+  /*
+  int variables - int <variable name> = <no>
+  double variables - double <variable name> = <no_with_decimal_point>
+   */
+
+  /*
+  String interpolation
+   */
+
+  /*
+  Type Methods and Properties
+   */
 }
