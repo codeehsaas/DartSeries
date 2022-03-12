b3214dd0e556fa49572bb50055989d79ae4a09c2
Code Ehsaas <codeehsaas@gmail.com>
2022-03-11 20:24:12 -0600

    Initial commit

---
+++ Operators-Loops-Flow/CE-ControlFlow-Loops-Quiz.dart
@@ -0,0 +1,10 @@
+void main() {
+  /*
+  Write program to convert 55 F to Celsius.
+  Use this formula, C = (F - 32) / 1.8
+  You've tempF = 55 and you calculate tempC
+  Print answer to 1 decimal place like this 55F is 12.777777C:
+  Hint - Use the 'fixed' method of double class
+  55F = 12.7C
+   */
+}
---
+++ Operators-Loops-Flow/CE-ControlFlow-Loops.dart
@@ -0,0 +1,35 @@
+void main() {
+  /*
+  Operator Precedence:
+   - Use parenthesis to improve readability
+   - Operators have precedence. For e.g * is higher than ==
+
+  Arithmetic:
+   - + - * / ~/ (int division) % (remainder) ++var var++ --var var--
+    += (compound assignment operator), Operator Precedence force with ()
+*/
+
+/*
+  Relational & Type Test:
+   - To test whether 2 objects represent the same thing
+   - >= > <= < as is is!
+   - They always return a bool value
+
+  Equality: == !=
+  Logical And, OR: && || (either one), Ternary Operator exp1 ? exp2 : exp3
+  - print ( x == y) //equal *note both are num
+  - print (x != y) // not equal
+  - Use with any expression that returns bool. Say myemail.com
+
+*/
+
+/*
+  Control Flow
+  if-else
+  for loop
+*/
+
+  var x = 10.0;
+  var y = 6;
+  print('x=$x, y=$y');
+}
