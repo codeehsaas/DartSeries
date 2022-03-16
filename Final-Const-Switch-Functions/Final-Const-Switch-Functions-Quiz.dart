00a21cb4f4e8465da5340976e224e6588cd0a46b
Code Ehsaas <codeehsaas@gmail.com>
2022-03-16 11:27:50 -0500

    Initial commit

---
+++ Final-Const-Switch-Functions/Final-Const-Switch-Functions-Quiz.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  /*
+  We'll create a simple Calculator and use Final/Enum/Switch
+  - Create an enum called Action with values -
+    Add, Subtract, Divide, Multiply
+  - Create final variables called num1 & num2 with values 25, 5
+  - Create variable opt with a value of Action, say Divide
+  - Use Switch statement to process the value and print the
+    result of the Action. So in this case you'll do Divide.
+   */
+
+  final num1 = 25;
+  final num2 = 5;
+  print('num1 $num1, num2 $num2');
+
+  //Add rest of the code below
+}
---
+++ Final-Const-Switch-Functions/Final-Const-Switch-Functions.dart
@@ -0,0 +1,51 @@
+void sayHello(String name, String greeting) {
+  print('$greeting, $name');
+}
+
+enum Snacks { Samosa, Barfi, Idli }
+
+void main() {
+  /*
+Final var - You know you will not change it again. Can only be set once.
+   Const - Compile time constant. Has to be set when declared.
+
+  final String petBreed;
+  const int hrsInDay;
+  petBreed = 'Ridgeback';
+  var petName = 'Zyraa';
+  print('Pet is $petBreed');
+
+Enumerated types - Use to represent easy-to-remember pre-defined const values
+  - enum Lang {Python, Dart, C++, Java}
+  - The .name property gives the string value of the enum
+  - Each value has an index property which returns value starting with 0
+  - You can get all these using .values property
+  - Use with switch with built-in warning if not handling all enum values
+  - Trailing comma helps with formatting in code
+
+Switch - Control flow statement
+- Use with int, string, or constants using ==
+- Each non-empty clause ends with break
+- Can use break,continue, throw, re-throw or return
+- Use default clause to execute code when nothing matches your variable
+- Example - Use snacks : Samosa, Gulabjamun, Barfi, Murukku, Dhokla, Idli
+
+Functions - Allow to keep often used code in one place.
+- Mostly should do something
+- Can be called multiple times, DRY principle
+- void() function means it returns no value
+- Simple function greetPet()
+- Update greetPet() with variables: String name, String greeting
+- Optional parameters come after required parameters. You can have Positional
+- Or Named Parameters but NOT Both!
+- The default value for optional parameters is null
+- Optional Positional Parameter are wrapped in [String greeting='Hello']
+- Named Parameters are optional unless marked as required.
+  - Such parameters are wrapped in {param1 value}
+  - When calling such functions, use paramName : value to call
+
+   */
+
+  var name = 'Zyraa';
+  print('Hello, $name 😊');
+}
