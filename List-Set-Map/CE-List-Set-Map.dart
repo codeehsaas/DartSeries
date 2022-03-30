bffdc0b29a79cdecf30b2a9257ed6f86a782c1e2
Code Ehsaas <codeehsaas@gmail.com>
2022-03-30 08:40:10 -0500

    Initial Commit

--- List-Set-Map/CE-List-Set-Map.dart
+++ List-Set-Map/CE-List-Set-Map.dart
@@ -1,17 +1,22 @@
 void main(List<String> args) {
   /*
-  Dynamic type
+  Dynamic & null type
   - Normally Dart can infer var, final or const variables
   - There's another type dynamic which can be used in specific cases
   - var x = 'Zyraa';
   - x = 10 <not allowed>
   - dynamic kuchBhi = 'Zyraa'
    */
+
   /*
   List - Also called Arrays in other languages
   - Index are always zero-based
   - [idx] also called using subscript operator
   - You can display and assign using [] operator
+  - You can add another list using the ...spread operator
+  - var pets = ['Tiger', 'Shera'];
+  - var petNames = <String>['Zyraa', 'Rover', 'Ran', 'Mindy', 'Hut', ...pets]
+  - Anonymous functions () =>
   - for (var e in list) {} will print the items at each stage with var e
  - List Methods -
    - first (Error: bad state on Empty list), last,
@@ -24,6 +29,8 @@ void main(List<String> args) {
    - The spread operator allows you to insert values if another list into 1st one
    - runtimeType will print the list <type>
    -whereType() will filter by type. toList will copy & preserve type
+    - var nos = [10, 5.5, 15, 2.8, 3.2, 'Zyraa', 'Turner'];
+  - print(nos.whereType<String>());
    -To create with right type, create a <int>[] within, add to itm and return it
    */
 
@@ -31,16 +38,22 @@ void main(List<String> args) {
   Set - An unordered collection of unique items
   - To create an empty set use <String>{}.
   - To add use the add, for single, or addAll to add from a list
-  - You can create directly using Set.from([..])
+  - You can create from a list using Set<String>.from([..])
+  - var petList = <String>['GSD', 'Labrador', 'Ridgeback', 'GSD', 'Rottweiler'];
+  - var petBreeds = Set<String>.from(petList);
+  - var anotherBreed = <String>{'Spaniel', 'Malinois', 'GSD', 'Ridgeback'};
   - Use contains() or containsAll() to check single or list items.
   - Use intersection to give you similar items from 2 sets
   -
    */
+
   /*
   Map
-  - Lets you store key-value pair. Key is unique, but value can be same
+  - Lets you store key-value pair. Key is unique, but Value can be same
   - Without type, you may create a Map<dynamic, dynamic>
   - For a key that isn't in a map, you get a null on return
+  -  var dayWeek = {'Mon': 1, 'Tue': 2, 'Wed': 3, 'Thu': 4, 'Fri': 5};
+  - Example: Find count of characters in String: Mary had a little lamb
   - Comprehension
    -   var squareMap = {for (var i in nos) 'Square of $i': i * i};
    */
